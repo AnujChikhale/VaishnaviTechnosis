8083e2c4ab0950906d8c17c877ea1f931f8f65ac
Anuj Chikhale <chikhaleanuj38@gmail.com>
2025-06-15 10:36:14 +0530

    sitemap updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { motion } from 'framer-motion';
 import { ChevronLeft, ChevronRight } from "lucide-react";
-
 import { useRef, useEffect, useState } from "react";
 
 const Navbar = () => (
